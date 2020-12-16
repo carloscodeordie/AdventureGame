c476693463eaaeb2355fc9b099969685d3e5011d
Carlos Macias <lmaciasgonzalez@gmail.com>
2020-12-16 13:08:10 -0600

    Refactor movement logic
    Refactor movement logic

---
+++ Assets/Scripts/Control/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private void Update()
+    {
+        // Verify if the user click on mouse left button
+        if (Input.GetMouseButton(0))
+        {
+            MoveToCursor();
+        }
+    }
+
+    private void MoveToCursor()
+    {
+        // Create ray to mouse position
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        // Variable that contains the click position
+        RaycastHit hit;
+
+        // Triggers a ray in mouse position
+        bool hasHit = Physics.Raycast(ray, out hit);
+
+        // Validate if the ray hit some valid terrain
+        if (hasHit)
+        {
+            // Move player to new destination
+            GetComponent<Mover>().MoveTo(hit.point);
+        }
+    }
+}
--- Assets/Scripts/Movement/Mover.cs
+++ Assets/Scripts/Movement/Mover.cs
@@ -16,40 +16,8 @@ public class Mover : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        ProcessMouseInput();
-    }
-
-    /**
-     * Process mouse input used to move the player in the screen
-     * */
-    private void ProcessMouseInput()
-    {
-        // Verify if the user click on mouse left button
-        if (Input.GetMouseButton(0))
-        {
-            MoveToCursor();
-        }
         // Update the movement animator
         UpdateAnimator();
-    }
-
-    private void MoveToCursor()
-    {
-        // Create ray to mouse position
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        
-        // Variable that contains the click position
-        RaycastHit hit;
-
-        // Triggers a ray in mouse position
-        bool hasHit = Physics.Raycast(ray, out hit);
-
-        // Validate if the ray hit some valid terrain
-        if (hasHit)
-        {
-            // Move player to new destination
-            SetTargetDestination(hit.point);
-        }
     }
 
     // This method changes the animator according the forward player speed
@@ -71,7 +39,7 @@ public class Mover : MonoBehaviour
     /**
      * Move player into target destination
      * */
-    private void SetTargetDestination(Vector3 destination)
+    public void MoveTo(Vector3 destination)
     {
         navMeshAgent.SetDestination(destination);
     }
