fe406874edce6916f5e0a111d6ee2f81ed72d1d3
Carlos Macias <lmaciasgonzalez@gmail.com>
2021-01-06 07:05:57 -0600

    Trigger the cinematics when the player reach to some area
    Trigger the cinematics when the player reach to some area

---
+++ Assets/Scripts/Cinematics/CinematicTrigger.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.Playables;
+using RPG.Control;
+
+namespace RPG.Cinematics
+{
+    public class CinematicTrigger : MonoBehaviour
+    {
+        bool isTriggered = false;
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.gameObject.tag == "Player" && !isTriggered)
+            {
+                GetComponent<PlayableDirector>().Play();
+                isTriggered = true;
+            }
+        }
+    }
+}
